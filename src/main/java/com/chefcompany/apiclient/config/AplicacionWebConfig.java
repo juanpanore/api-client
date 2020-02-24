1a7fe1a18321135dfb0a46b5343c49782dde26ee
juannobl <jnorenab@sura.com.co>
2020-02-23 23:50:06 -0500

    se agrega permitir PUT a DELETE de origen diferente

--- src/main/java/com/chefcompany/apiclient/config/AplicacionWebConfig.java
+++ src/main/java/com/chefcompany/apiclient/config/AplicacionWebConfig.java
@@ -15,7 +15,7 @@ public class AplicacionWebConfig {
 	        return new WebMvcConfigurerAdapter() {
 	            @Override
 	            public void addCorsMappings(CorsRegistry registry) {
-	                registry.addMapping("/api/v1/**");
+	                registry.addMapping("/api/v1/**").allowedMethods("DELETE","PUT","POST","GET");
 	            }
 	        };
 	    }

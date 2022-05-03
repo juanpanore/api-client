55eeb949adce9af26d85e6fafb1aea43369bce52
juannobl <jpnorena@sura.com.co>
2022-05-02 22:46:02 -0500

    se agrega proxy con quotaguard

--- src/main/java/com/chefcompany/apiclient/config/AplicacionWebConfig.java
+++ src/main/java/com/chefcompany/apiclient/config/AplicacionWebConfig.java
@@ -21,33 +21,6 @@ public class AplicacionWebConfig {
 	        };
 	    }
 
-	@Bean
-		public static void setProxy() throws IOException {
-			URL proxyUrl = new URL(System.getenv("QUOTAGUARDSHIELD_URL"));
-			String userInfo = proxyUrl.getUserInfo();
-			String user = userInfo.substring(0, userInfo.indexOf(':'));
-			String password = userInfo.substring(userInfo.indexOf(':') + 1);
-
-			URLConnection conn = null;
-			System.setProperty("http.proxyHost", proxyUrl.getHost());
-			System.setProperty("http.proxyPort", Integer.toString(proxyUrl.getPort()));
-
-			Authenticator.setDefault(new Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(user, password.toCharArray());
-				}
-			});
-
-			URL url = new URL("https://ip.quotaguard.com");
-			conn = url.openConnection();
-			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-			String inputLine;
-			while ((inputLine = in.readLine()) != null)
-				System.out.println(inputLine);
-
-			in.close();
-		}
 
 
 }

99f29979fcc0b5c89d66fae2b7b940bb18c5247e
juannobl <jpnorena@sura.com.co>
2022-05-02 22:10:26 -0500

    se agrega proxy con quotaguard

--- src/main/java/com/chefcompany/apiclient/config/AplicacionWebConfig.java
+++ src/main/java/com/chefcompany/apiclient/config/AplicacionWebConfig.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+import java.net.*;
+import java.io.*;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -19,6 +20,34 @@ public class AplicacionWebConfig {
 	            }
 	        };
 	    }
-	    
+
+	@Bean
+		public static void setProxy() throws IOException {
+			URL proxyUrl = new URL(System.getenv("QUOTAGUARDSHIELD_URL"));
+			String userInfo = proxyUrl.getUserInfo();
+			String user = userInfo.substring(0, userInfo.indexOf(':'));
+			String password = userInfo.substring(userInfo.indexOf(':') + 1);
+
+			URLConnection conn = null;
+			System.setProperty("http.proxyHost", proxyUrl.getHost());
+			System.setProperty("http.proxyPort", Integer.toString(proxyUrl.getPort()));
+
+			Authenticator.setDefault(new Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(user, password.toCharArray());
+				}
+			});
+
+			URL url = new URL("https://ip.quotaguard.com");
+			conn = url.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+			String inputLine;
+			while ((inputLine = in.readLine()) != null)
+				System.out.println(inputLine);
+
+			in.close();
+		}
+
 
 }

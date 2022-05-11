06941ec42ca98dd4c1d417d851e4f94676e1445e
juannobl <jpnorena@sura.com.co>
2022-05-10 21:55:19 -0500

    se agrega servicio firmas

---
+++ src/main/java/com/chefcompany/apiclient/controllers/firmas/FirmaController.java
@@ -0,0 +1,30 @@
+package com.chefcompany.apiclient.controllers.firmas;
+
+
+import com.chefcompany.apiclient.domain.firma.Firma;
+import com.chefcompany.apiclient.service.firma.FirmaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+@RequestMapping("api/v1/rest")
+
+
+public class FirmaController {
+
+        @Autowired
+        private FirmaService firmaService;
+
+        @GetMapping("/firmas")
+        public Mono<Firma> get(@RequestParam(required = true) String correoUsuario){
+
+            return firmaService.get(correoUsuario);
+        }
+
+
+}
